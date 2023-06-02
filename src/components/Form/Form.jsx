266b878ac22257c3f53828a43ea28f11f066bc25
Vladimir <vzangi@yandex.ru>
2023-06-02 14:58:47 +0300

    fix tg.webapp

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -18,9 +18,9 @@ const Form = () => {
   }, [country, street, subject])
 
   useEffect(() => {
-    tg.WebApp.onEvent('mainButtonClicked', onSendData)
+    tg.onEvent('mainButtonClicked', onSendData)
     return () => {
-      tg.WebApp.offEvent('mainButtonClicked', onSendData)
+      tg.offEvent('mainButtonClicked', onSendData)
     }
   }, [onSendData])
 
