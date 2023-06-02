d9b8178608db34a86b7f0d4160cc5829f2cf0e9e
Vladimir <vzangi@yandex.ru>
2023-06-02 13:38:55 +0300

    form href

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './ProductList.css'
 
 const ProductList = () => {
-  return <div>Products</div>
+  return (
+    <div>
+      <a href="/form">Form</a>
+    </div>
+  )
 }
 
 export default ProductList
